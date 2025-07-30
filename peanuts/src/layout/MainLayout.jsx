c2271a65528df5780fe4202a6222c04d13b58e0e
sonamkumari1 <sonamkumari63928@gmail.com>
2025-07-30 11:33:53 +0530

    add watsappbadge

--- peanuts/src/layout/MainLayout.jsx
+++ peanuts/src/layout/MainLayout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
+import WhatsAppBadge from "../components/WhatsAppBadge";
 
 function MainLayout() {
   return (
     <div>
       <Header />
       <Outlet />
+      <WhatsAppBadge />
       <Footer />
     </div>
   );
